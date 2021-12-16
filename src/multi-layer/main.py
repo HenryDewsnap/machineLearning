7b8173681dda2e64c6cd8263699e8b84bbf7c6fd
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-16 22:54:58 +0000

    Update main.py

--- src/multi-layer/main.py
+++ src/multi-layer/main.py
@@ -1,73 +1,86 @@
+import matplotlib
 import random
-import math
 import json
+import math
 
-##architecture example:
-##[5,3,3,2]
-##5 input neurons.
-##3 hidden neurons.
-##3 hidden neurons.
-##2 output neurons.
-
-
-def loadJson(filePath):
-    try: return json.load(open(filePath, "r"))
-    except: exit(f"File {filePath} Not Found")
-
-def generate_weights(count):
-    array = []
-    for c in range(count): array.append(random.uniform(0,1))
-    return array
-
+####    FUNCTIONS    ####
 def sigmoid(x): return 1/(1+math.exp(-x))
+#This value x must have been run through the sigmoid function first!
 def sigmoid_gradient(x): return x * (1 - x)
 
+def generate_weights(quantity, value_range):
+    output = []
+    for i in range(quantity): output.append(random.uniform(value_range[0], value_range[1]))
+    return output
+
+def matrix_maths(A, B, operator):
+    output = []
+    if len(A) == len(B):
+        for ic in range(len(A)): output.append(eval(f"{A[ic]} {operator} {B[ic]}"))
+    return output
+
+#### END OF FUNCTIONS ####
+
+
+
 class layer:
-    def __init__(self, neuron_count, forward_connections):
-        self.forward_connections = forward_connections
-        self.neurons = [0]*neuron_count
-        self.synaptic_weights = []
-        for fc in range(self.forward_connections): self.synaptic_weights.append(generate_weights(len(self.neurons)))
+    def __init__(self, neuron_count, forward_count):
+        self.forward_count = forward_count
+        self.neuron_count  = neuron_count
 
-    def calculate_output(self, inputs):
-        self.neurons = inputs
-        outputs = []
-        for forward_connection in range(self.forward_connections):
-            outputs.append(0)
-            for neuron_ic in range(len(self.neurons)):
-                outputs[forward_connection] += self.neurons[neuron_ic] * self.synaptic_weights[forward_connection][neuron_ic]
-        for output_ic in range(len(outputs)): outputs[output_ic] = sigmoid(outputs[output_ic])
-        return outputs
+        self.neurons = [0] * neuron_count
+        self.outputs = [0] * forward_count
+        self.weights = []
+        for i in range(forward_count): self.weights.append(generate_weights(neuron_count, [-1, 1]))
 
-    def train_layer(self, desired_outputs, output):
-        for forward_connection in range(self.forward_connections):
-            error = desired_outputs[forward_connection] - output[forward_connection]
-            adjustments = error * sigmoid_gradient(output[forward_connection])
+    def compile_outputs(self):
+        output_neurons = [0] * self.forward_count
+        for forward_connection in range(self.forward_count):
+            for layer_neuron in range(self.neuron_count):
+                output_neurons[forward_connection] += self.neurons[layer_neuron] * self.weights[forward_connection][layer_neuron]
+        self.outputs.clear()
+        for output in output_neurons: self.outputs.append(sigmoid(output))
 
-            for neuron_ic in range(len(self.neurons)):
-                self.synaptic_weights[forward_connection][neuron_ic] += self.neurons[neuron_ic] * adjustments
+    def update_inputs(self, inputs): self.neurons = inputs
 
+    def return_output(self): return self.outputs
 
-    def display_layer(self): print(f"\nInfo:\n- Layer Neurons:{len(self.neurons)}\n- Inheriting Neurons: {self.forward_connections}\n")
-
-
-
-class neuralNet:
-    def __init__(self, architecture, iterations, trainingData):
-        self.layers = []
+class network:
+    def __init__(self, architecture):
+        self.training_data = {}
+        self.architecture = architecture
+        self.network_layers = []
         for layer_ic in range(len(architecture)-1):
-            self.layers.append(layer(architecture[layer_ic], architecture[layer_ic+1]))
+            self.network_layers.append(layer(architecture[layer_ic], architecture[layer_ic+1]))
 
+    def feed_forward(self, inputs):
+        previous_output = inputs
+        for layer_ic in range(len(self.network_layers)):
+            self.network_layers[layer_ic].update_inputs(previous_output)
+            self.network_layers[layer_ic].compile_outputs()
+            previous_output = self.network_layers[layer_ic].return_output()
+        return previous_output
 
-    def execute_layers(self, inputs):
-        previous_layer = inputs
-        print(f"Inputs: {inputs}")
-        for layer in self.layers:
-            previous_layer = layer.calculate_output(previous_layer)
-        print(f"Outputs: {previous_layer}")
+    def back_propagation(self, iterations):
+        for iteration in range(iterations):
 
+            for training_set in self.training_data:
+                current_set = self.training_data[training_set]
+
+                self.feed_forward(current_set['input']) ##Initialises each layer with output data by feeding an input through the network.
+                
+                training_outputs = current_set['output']
+                training_inputs  = current_set['input']
+
+                for layer in reversed(self.network_layers[1::]):
+                    error = matrix_maths(training_outputs, layer.outputs, "-")
+                    
+
+    def load_training_data(self, path):
+        self.training_data = json.load(open(path, "r"))
 
 
 if __name__ == "__main__":
-    a = neuralNet([5,3,3,2], 10000, "training_data.json")
-    a.execute_layers([0,1,0,1,0])
+    NN = network([4,8,8,6,2])
+    NN.load_training_data("training_data.json")
+    NN.back_propagation(1)
